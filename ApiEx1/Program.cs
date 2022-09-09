e2a0f8335ba7e2e1fb55f51eb9d8b4220a4ed8c5
dfish19 <dfish19@yahoo.com>
2022-09-09 14:46:02 -0500

    initial

--- ApiEx1/Program.cs
+++ ApiEx1/Program.cs
@@ -9,13 +9,13 @@ for (int i = 0; i < 5; i++)
     var kanye = "https://api.kanye.rest";
     var kResponse = Kclient.GetStringAsync(kanye).Result;
     var kQuote = JObject.Parse(kResponse);   //.GetValue("quote").ToString();
-    Console.WriteLine($"Mr. West# {kQuote["quote"]}");
+    Console.WriteLine($"(Mr. West) {kQuote["quote"]}");
 
     var Rclient = new HttpClient();
     var ron = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
     var rResponse = Rclient.GetStringAsync(ron).Result;
     var rQuote = JArray.Parse(rResponse); // ToString();
-    Console.WriteLine($"Mr. Swanson# {rQuote[0]}");
+    Console.WriteLine($"(Mr. Swanson) {rQuote[0]}");
     Console.WriteLine();
 }
 
